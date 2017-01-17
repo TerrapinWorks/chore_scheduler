91e590b4d31d1abc63f1e949272bde16ea24b54b
blepfo <blepfo@gmail.com>
2017-01-17 18:39:23 +0000

    Upload from JSON to sheet

--- src/sheets_interface.py
+++ src/sheets_interface.py
@@ -50,27 +50,27 @@ def update_json():
 	chores_update = get_data('chores')
 	return (candidates_update and chores_update)
 
-def get_data(data='candidates', save=True):
+def get_data(data_type='candidates', save=True):
 	""" Get information from spreadsheet and save to local JSON
 	Candidates and Chores with no name are skipped
 	Blank sheets count as a successful get_data()
 	Returns true on success
 	"""
 	api_object.log_message(sheet_id, 'GET: downloading DATA'
-				' = ' + data + ' to the Pi', LOGFILE)
+				' = ' + data_type + ' to the Pi', LOGFILE)
 	# Set range/files to use depending on whether function is getting
 	# candidates or chores data
-	sheets_range = CANDIDATES_RANGE if data == 'candidates' else CHORES_RANGE
-	data_file = CANDIDATES_FILE if data == 'candidates' else CHORES_FILE
-	backup_file = CANDIDATES_BACKUP if data == 'candidates' else CHORES_BACKUP
+	sheet_range = CANDIDATES_RANGE if data_type == 'candidates' else CHORES_RANGE
+	data_file = CANDIDATES_FILE if data_type == 'candidates' else CHORES_FILE
+	backup_file = CANDIDATES_BACKUP if data_type == 'candidates' else CHORES_BACKUP
 	result = sheets_service.spreadsheets().values().get( 
-			spreadsheetId=sheet_id, range=sheets_range).execute()
+			spreadsheetId=sheet_id, range=sheet_range).execute()
 	values = result.get('values', [])
 	if not values:
 		# No data on the sheet
 		# Clear candidates.json and count as success
 		api_object.log_message(sheet_id, "NOTE in get_data() for " + 
-					"DATA = " + data + ": No data on sheet", LOGFILE)
+					"DATA = " + data_type + ": No data on sheet", LOGFILE)
 		# Save backup
 		system("cp " + data_file + " " + backup_file)
 		if save:
@@ -79,14 +79,48 @@ def get_data(data='candidates', save=True):
 		return True 
 	else:
 		# Spreadsheet contains data. 
-		if data=='candidates':
-			data_dict = parse_candidate_data(values)
-		elif data =='chores':
-			data_dict = parse_chore_data(values)
-		else:
+		# Make sure that the requested data is one of the two allowed types
+		if not (data_type == 'candidates' or data_type == 'chores'):
 			api_object.log_message(sheet_id, "ERROR in get_data(): "
 						"DATA = " + data + " is invalid data type", LOGFILE)
 			return False
+		data_dict = []
+		for row_num, row in enumerate(values):
+			try:
+				if row[0] != '':
+					if data_type == 'candidates':
+						email = row[1] if len(row) > 1 else None
+						candidate = {"name" : row[0], "email" : email}
+						# Get start times
+						weekdays = ["mon", "tues", "wed", "thurs", "fri"]
+						for day_num in range(0, len(weekdays)):
+							candidate[weekdays[day_num]] = \
+										row[day_num + 2] if len(row) > (day_num + 2) else None
+						candidate["assigned_chores"] = row[7] if len(row) > 7 else None
+						data_dict.append(candidate)
+					elif data_type == 'chores':
+						# Set default completion status if cell is empty
+						completion_frequency = row[1] if ((len(row) > 1) and row[1] != '') \
+																						else DEFAULT_COMPLETION_FREQUENCY
+						# Set fields to None if empty
+						assignees = row[2] if ((len(row) > 2) and row[2] != '') else None
+						assignment_time = row[3] if ((len(row) > 3) and row[3] != '') else None
+						# Set completion status to false if cell is empty
+						completion_status = row[4] if len(row) > 4 else "FALSE"
+						chore = {
+											"name" : row[0],
+											"completion_frequency" : completion_frequency,
+											"assignees" : assignees,
+											"assignment_time" : assignment_time,
+											"completion_status" : completion_status
+										}
+						data_dict.append(chore)
+				else:
+					api_object.log_message(sheet_id, "ERROR in get_data(): "
+								+ data_type + " does not have a name on line %d" % (row_num + 2))
+			except IndexError:
+				api_object.log_message(sheet_id, "INDEX ERROR in get_data(): "
+						"DATA = " + data_type + " on row %d" % (row_num + 2))
 		# Save backup
 		system("cp " + data_file + " " + backup_file)
 		# Store candidate information
@@ -100,63 +134,7 @@ def get_data(data='candidates', save=True):
 							"Error saving Candidate info candidates.json.", LOGFILE)
 				return False
 		return True
-		
-def parse_candidate_data(values):
-	""" Parses values from Candidates tab of the spreadsheet
-	Returns dictionary with candidate information
-	"""
-	candidates = []
-	for row_num, row in enumerate(values):
-		try:
-			if row[0] != '':
-				email = row[1] if len(row) > 1 else None
-				candidate = {"name" : row[0], "email" : email}
-				# Get start times
-				weekdays = ["mon", "tues", "wed", "thurs", "fri"]
-				for day_num in range(0, len(weekdays)):
-					candidate[weekdays[day_num]] = \
-								row[day_num + 2] if len(row) > (day_num + 2) else None
-				candidate["assigned_chores"] = row[7] if len(row) > 7 else None
-			candidates.append(candidate)
-		except IndexError:
-			print("Index Error: In parse_candidate_data") 
-			api_object.log_message(sheet_id, "INDEX ERROR while parsing"
-					" candidates: for candidate on row %d" % (row_num + 2))
-	return candidates
 
-def parse_chore_data(values):
-	""" Parses values from Chores tab of the spreadsheet
-	Returns dictionary with chore information
-	"""
-	chores = []
-	for row_num, row in enumerate(values):
-		try:
-			if row[0] != '':
-				# Set default completion status if cell is empty
-				completion_frequency = row[1] if ((len(row) > 1) and row[1] != '') \
-																				else DEFAULT_COMPLETION_FREQUENCY
-				# Set fields to None if empty
-				assignees = row[2] if ((len(row) > 2) and row[2] != '') else None
-				assignment_time = row[3] if ((len(row) > 3) and row[3] != '') else None
-				# Set completion status to false if cell is empty
-				completion_status = row[4] if len(row) > 4 else "FALSE"
-				chore = {
-									"name" : row[0],
-									"completion_frequency" : completion_frequency,
-									"assignees" : assignees,
-									"assignment_time" : assignment_time,
-									"completion_status" : completion_status
-								}
-				chores.append(chore)
-			else:
-				api_object.log_message(sheet_id, "ERROR in update_json(): "
-							"Chore does not have a name on line %d" % (row_num + 2))
-		except IndexError:
-			print("Index Error: Error adding chore")
-			api_object.log_message(sheet_id, "INDEX ERROR in get_chores(): "
-						"for chore on row %d" % (row_num + 2))
-	return chores
-	
 
 """ --- Functions to update spreadsheet with data stored locally ---"""
 def update_spreadsheet():
@@ -167,39 +145,72 @@ def update_spreadsheet():
 	On the chores tab - Assignee(s), Assignment Time, Completion Status
 	Returns True only if both updates are successful
 	"""
-	candidates_update = set_data(data='candidates')
-	chores_update = set_data(data='chores')
+	candidates_update = set_data(data_type='candidates')
+	chores_update = set_data(data_type='chores')
 	return (candidates_update and chores_update)
 
-def set_data(data='candidates'):
+def set_data(data_type='candidates'):
 	""" Reset Candidate information on spreadsheet
 	Deletes current information in Candidates tab
 	and replaces with current contents of candidates.json
+	Returns true on success
 	"""
-	api_object.log_message(sheet_id, 'SET: uploading DATA = '
-				+ data + ' from the Pi', LOGFILE)
+	api_object.log_message(sheet_id, 'BACKUP: Backing up data to the PI',	LOGFILE)
 	# Set range/files to use depending on whether function is getting
 	# candidates or chores data
-	sheets_range = CANDIDATES_RANGE if data == 'candidates' else CHORES_RANGE
-	data_file = CANDIDATES_FILE if data == 'candidates' else CHORES_FILE
+	sheet_range = CANDIDATES_RANGE if data_type == 'candidates' else CHORES_RANGE
+	data_file = CANDIDATES_FILE if data_type == 'candidates' else CHORES_FILE
 	# Save backup of what is currently on the sheet
-	get_data(data=data, save=False)
+	get_data(data_type=data_type, save=False)
 	# Read candidate information
 	try:
 		with open(data_file, 'r') as dataJSON:
 			data = json.loads(dataJSON.read())
 	except:
 		api_object.log_message(sheet_id, "ERROR in set_data for DATA"
-					" = " + data + ": data file is empty: " + data_file, LOGFILE)
+					" = " + data_type + ": data file is empty: " + data_file, LOGFILE)
 		return False
+	api_object.log_message(sheet_id, 'SET: uploading DATA = '
+				+ data_type + ' from the Pi', LOGFILE)
 	# Clear current sheet contents
-	api_object.clear_sheet_range(sheet_id, sheets_range)
+	api_object.clear_sheet_range(sheet_id, sheet_range)
 	# Sheet contents with JSON contents
-	json_to_sheet(data_file)
+	return json_to_sheet(data_type, data_file)
 
-def json_to_sheet(json_file):
-	pass
-	
+def json_to_sheet(data_type, json_file):
+	""" Upload from either candidates.json or chores.json to the sheet
+	We assume that this is run after clear_sheet_range() so the sheet
+	will be empty
+	"""
+	# Set range based on data being uploaded
+	sheet_range = CANDIDATES_RANGE if data_type == 'candidates' else CHORES_RANGE
+	# Load data from JSON into dictionary
+	with open(json_file, 'r') as dataJSON:
+		data_dict = json.loads(dataJSON.read())
+	# Make sure one of the two allowed types of data is being uploaded
+	if data_type != 'candidates' and data_type != 'chores':
+		api_object.log_message(sheet_id, "ERROR in json_to_sheet(): "
+						"DATA = " + data + " is invalid data type", LOGFILE)
+		return False
+	# Upload to the sheet
+	values = []
+	for data in data_dict:
+		if data_type == 'candidates':
+			values.append(
+				[data['name'], data['email'], data['mon'], data['tues'],
+				 data['wed'], data['thurs'], data['fri'],
+				 data['assigned_chores']]
+			)
+		elif data_type == 'chores':
+			values.append(
+				[data['name'], data['completion_frequency'],
+				 data['assignees'], data['assignment_time'], 
+				 data['completion_status']]
+			)
+	body = {'values': values}
+	sheets_service.spreadsheets().values().update (
+				spreadsheetId=sheet_id, range=sheet_range,
+				valueInputOption='RAW', body=body).execute()
 
 
 if __name__ == '__main__':
